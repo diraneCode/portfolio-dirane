62742fb144f3c52a25476d0fda8b59a92a829a10
diranecode <diranecode@gmail.com>
2025-09-03 17:20:49 +0100

    ajout du favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,13 @@ import Navbar from "./components/Navbar"
 import "./globals.css"
 import { Orbitron } from "next/font/google"
 
-const orbitron = Orbitron({subsets:["latin"]})
+const orbitron = Orbitron({ subsets: ["latin"] })
 
 export const metadata = {
   title: `Dirane • Portfolio`,
   description: "Portfolio de développeur et designer d'applications",
   icons: {
-    icon:'/dirane-logo.ico'
+    icon: '/dirane-logo.ico'
   }
 }
 
@@ -20,6 +20,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={`${orbitron.className} bg-gray-900 text-white`}>
         <Navbar />
         {children}
